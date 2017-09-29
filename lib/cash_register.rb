b718e6bf3676437598adff5955ef6178faf601af
Emily Morgan <emorgan05@gmail.com>
2017-09-29 18:22:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,10 @@
 class CashRegister
 
-  def initialize
+  def initialize(discount = 0)
     @total = 0
+    @discount = discount
   end
 
-  def discount(discount)
-    @discount = discount
+  def discount
   end
 end
