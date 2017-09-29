dc2347845db906cd6328477163be8c6db102ff77
Emily Morgan <emorgan05@gmail.com>
2017-09-29 18:40:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,7 +17,7 @@ class CashRegister
 
   def apply_discount
     percent = @discount/100.to_f
-    @total = @total - (@total * percent)
+    @total = @total - (@total * percent).to_i
     "After the discount, the total comes to #{@total}."
   end
 end
